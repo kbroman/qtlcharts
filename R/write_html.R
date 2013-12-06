633cd559f4e0160f24052e278806e1fe57f7adbf
Karl Broman <kbroman@gmail.com>
2013-12-06 15:43:09 -0600

    Add some initial functions for writing an html file

---
+++ R/write_html.R
@@ -0,0 +1,58 @@
+## write_html
+## Karl W Broman
+
+#' Write the initial part of an html file
+#'
+#' @param file Character vector with file name, will be over-written
+#' @param title Character vector with title for html page
+#' @return Returns the file name (invisibly)
+#' @keywords IO
+#' @examples
+#' \dontrun{file <- write_html_top(title="QTL chart")}
+write_html_top <-
+function(file=tempfile(tmpdir=tempdir(), fileext=".html"), title="qtlcharts chart")
+{
+  cat('<!DOCTYPE html>\n<html lang="en">\n<head>\n    <meta charset="utf-8">\n',
+      file=file)
+
+  cat('    <title>', title, '</title>\n', file=file, append=TRUE)
+
+  # return file name invisibly
+  invisible(file)
+}
+
+#' Append css link to an html file
+#'
+#' @param file File to which the link will be written
+#' @param cssfile Path to the cssfile
+#' @return None (invisible NULL)
+#' @keywords IO
+append_html_csslink <-
+function(file, cssfile)    
+{
+  cat('    <link rel=stylesheet type="text/css" href="', file=file, append=TRUE)
+  cat(cssfile, file=file, append=TRUE)
+  cat('</link>\n', file=file, append=TRUE)
+
+  invisible(NULL)
+}
+
+#' Append javascript link to an html file
+#'
+#' @param file File to which the link will be written
+#' @param jsfile Path to the jsfile
+#' @param charset Optional character set
+#' @return None (invisible NULL)
+#' @keywords IO
+#' @examples
+#' \dontrun{append_html_jslink("index.html", "d3.min.js", "utf-8")}
+append_html_jslink <-
+function(file, jsfile, charset)
+{
+  cat('    <script ', file=file, append=TRUE)
+  if(!missing(charset))
+    cat('charset="', charset, '" ', file=file, append=TRUE)
+  cat('type="text/javascript" src="', jsfile, "></script>\n", file=file, append=TRUE)
+
+  invisible(NULL)
+}

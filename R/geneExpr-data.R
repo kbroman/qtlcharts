bcdfef4e3faaef45ce226ab6f50de94884542fba
Karl Broman <kbroman@gmail.com>
2013-12-07 00:37:50 -0600

    Add example gene expression data

---
+++ R/geneExpr-data.R
@@ -0,0 +1,12 @@
+#' @name geneExpr
+#' @title Anonymized gene expression data
+#' @description An anonymized set of gene expression values, for 100
+#' genes all influenced by a common locus, plus a vector of genotypes
+#' for the 491 individuals.
+#' @docType data
+#' @usage geneExpr
+#' @format A list containing a matrix \code{expr} with the gene
+#' expression data plus a vector \code{genotype} with the genotypes.
+#' @author Karl W Broman, 2013-05-16
+#' @keywords datasets
+NULL

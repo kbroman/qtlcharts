7fb34232d97ec36cfdad3c8dec233d66872883a5
Karl Broman <kbroman@gmail.com>
2013-12-06 23:15:31 -0600

    Add "gratuitous" to the legend for iplotScanone

--- R/iplotScanone.R
+++ R/iplotScanone.R
@@ -48,7 +48,7 @@ function(output, lodcolumn=1, file, onefile=FALSE, openfile=TRUE,
   append_html_jscode(file, 'data = ', scanone2json(output), ';\n\n', 'iplotScanone(data);')
 
   append_html_p(file, 'Hover over marker positions on the LOD curve to see the marker names. ',
-                'Click on a marker for a bit of animation.', class='legend')
+                'Click on a marker for a bit of gratuitous animation.', class='legend')
 
   append_html_bottom(file)
 

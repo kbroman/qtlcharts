173c94c0b8606412775a8762cb90ed8809b62351
Karl Broman <kbroman@gmail.com>
2014-03-03 13:40:16 -0600

    scatterplot/.../create_test_data.R: remove dependence on the R/broman package

--- inst/panels/scatterplot/test/create_test_data.R
+++ inst/panels/scatterplot/test/create_test_data.R
@@ -1,11 +1,16 @@
 # create test data in JSON format
 
 set.seed(27806353)
-library(broman)
-dat <- rmvn(200, c(2, 5, 10),
-            rbind(c(1, 0.5*2, 0.2*3), 
-                  c(0.5*2, 4, 0.8*3*2),
-                  c(0.2*3, 0.8*3*2, 9)))
+# simulate MVN data
+V <- rbind(c(1, 0.5*2, 0.2*3),
+           c(0.5*2, 4, 0.8*3*2),
+           c(0.2*3, 0.8*3*2, 9))
+D <- chol(V)
+mu <- c(2, 5, 10)
+p <- length(mu)
+n <- 200
+dat <- matrix(rnorm(n*p), ncol=p) %*% D +
+  rep(mu, rep(n,p))
 
 dat[1:4,3] <- NA
 dat[4:9,2] <- NA

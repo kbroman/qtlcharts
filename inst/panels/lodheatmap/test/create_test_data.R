2ca624c3b9810dffa990ec0ac0142da142774557
Karl Broman <kbroman@gmail.com>
2014-03-25 09:38:17 -0500

    test data for lodheatmap

--- inst/panels/lodheatmap/test/create_test_data.R
+++ inst/panels/lodheatmap/test/create_test_data.R
@@ -1,12 +1,7 @@
 # create test data in JSON format
 
-n <- 101
-x <- y <- seq(-pi, pi, len=n)
-z <- matrix(ncol=n, nrow=n)
-for(i in seq(along=x))
-  for(j in seq(along=y))
-    z[i,j] <- sin(x[i]) + cos(y[j])
-
-library(RJSONIO)
-cat(RJSONIO::toJSON(list(x=x, y=y, z=z)),
-    file="data.json")
+library(qtlcharts)
+data(grav)
+grav <- calc.genoprob(grav, step=2)
+out <- scanone(grav, method="hk", phe=seq(1, 241, by=20))
+cat(qtlcharts:::scanone2json(out), file="data.json")

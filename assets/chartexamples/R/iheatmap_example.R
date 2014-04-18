07a23291df208e145e274860160f59b369424cec
Karl Broman <kbroman@gmail.com>
2014-04-18 00:18:17 -0500

    Mention source of example function for iheatmap

--- assets/chartexamples/R/iheatmap_example.R
+++ assets/chartexamples/R/iheatmap_example.R
@@ -10,6 +10,9 @@ for(i in seq(along=x))
   for(j in seq(along=y))
     z[i,j] <- x[i]*y[j]*exp(-x[i]^2 - y[j]^2)
 
+# Example function from Dmitry Pelinovsky
+# http://dmpeli.mcmaster.ca/Matlab/Math1J03/LectureNotes/Lecture2_5.htm
+
 # remove the target file, if it exists
 file <- "../iheatmap_example.html"
 if(file.exists(file)) unlink(file)

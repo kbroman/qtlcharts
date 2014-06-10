531f2c50d1199ce09e93186eeb0604ac675443f9
Karl Broman <kbroman@gmail.com>
2014-06-10 16:01:51 -0500

    Revise jsonlite-related tests for new version of jsonlite package

--- inst/tests/test-chartopts.R
+++ inst/tests/test-chartopts.R
@@ -1,7 +1,7 @@
 
 context("chartOpts")
 
-tmpf <- function(opts, digits=2) toJSON(opts4json(opts), digits=digits)
+tmpf <- function(opts, digits=2) as.character(toJSON(opts4json(opts), digits=digits))
 addquotes <- function(x) paste0("\"", x, "\"")
 
 
@@ -10,7 +10,7 @@ test_that("conversion of chartOpts to JSON", {
   expect_equal(tmpf(NULL), "null")
 
   input01 <- c(xlab="x axis label", ylab="y axis label")
-  output01 <- paste("{", paste(addquotes(names(input01)), ":", addquotes(input01), collapse=", "), "}")
+  output01 <- "{\"xlab\":\"x axis label\",\"ylab\":\"y axis label\"}"
   expect_equal(tmpf(input01), output01)
 
   input02 <- list(xlab="x axis label", ylab="y axis label")
@@ -18,7 +18,7 @@ test_that("conversion of chartOpts to JSON", {
   expect_equal(tmpf(input02), output02)
 
   input03 <- list(xlab="x axis label", ylab="y axis label", zlab=NULL, margins=list(left=2, right=50))
-  output03 <- "{ \"xlab\" : \"x axis label\", \"ylab\" : \"y axis label\", \"zlab\" : null, \"margins\" : { \"left\" : 2, \"right\" : 50 } }"
+  output03 <- "{\"xlab\":\"x axis label\",\"ylab\":\"y axis label\",\"zlab\":null,\"margins\":{\"left\":2,\"right\":50}}"
   expect_equal(tmpf(input03), output03)
 
 })
@@ -27,7 +27,7 @@ test_that("use of add2chartOpts", {
 
   input04 <- list(x="x", y="y")
   input04 <- add2chartOpts(input04, chartdivid="chart")
-  output04 <- "{ \"x\" : \"x\", \"y\" : \"y\", \"chartdivid\" : \"chart\" }"
+  output04 <- "{\"x\":\"x\",\"y\":\"y\",\"chartdivid\":\"chart\"}"
   expect_equal(tmpf(input04), output04)
 
   input05 <- c(x="x", y="y")
@@ -37,7 +37,7 @@ test_that("use of add2chartOpts", {
 
   input06 <- list(x="x", y="y")
   input06 <- add2chartOpts(input06, title="blah", chartdivid="chart")
-  output06 <- "{ \"x\" : \"x\", \"y\" : \"y\", \"title\" : \"blah\", \"chartdivid\" : \"chart\" }"
+  output06 <- "{\"x\":\"x\",\"y\":\"y\",\"title\":\"blah\",\"chartdivid\":\"chart\"}"
   expect_equal(tmpf(input06), output06)
 
   input07 <- c(x="x", y="y")
@@ -50,19 +50,19 @@ test_that("use of add2chartOpts", {
 test_that("small things", {
 
   input08 <- list(x="a", y=NA)
-  output08 <- "{ \"x\" : \"a\", \"y\" : null }"
+  output08 <- "{\"x\":\"a\",\"y\":null}"
   expect_equal(tmpf(input08), output08)
 
   input09 <- list(x=NA, y="a")
-  output09 <- "{ \"x\" : null, \"y\" : \"a\" }"
+  output09 <- "{\"x\":null,\"y\":\"a\"}"
   expect_equal(tmpf(input09), output09)
 
   input10 <- list(x=1, y=NA)
-  output10 <- "{ \"x\" : 1, \"y\" : null }"
+  output10 <- "{\"x\":1,\"y\":null}"
   expect_equal(tmpf(input10), output10)
 
   input11 <- list(x=NA, y=1)
-  output11 <- "{ \"x\" : null, \"y\" : 1 }"
+  output11 <- "{\"x\":null,\"y\":1}"
   expect_equal(tmpf(input11), output11)
 
   input12 <- c(x="a", y=NA)
@@ -74,11 +74,11 @@ test_that("small things", {
   expect_equal(tmpf(input13), output13)
 
   input14 <- c(x=1, y=NA)
-  output14 <- "{ \"x\" : 1, \"y\" : \"NA\" }"
+  output14 <- "{\"x\":1,\"y\":\"NA\"}"
   expect_equal(tmpf(input14), output14)
 
   input15 <- c(x=NA, y=1)
-  output15 <- "{ \"x\" : \"NA\", \"y\" : 1 }"
+  output15 <- "{\"x\":\"NA\",\"y\":1}"
   expect_equal(tmpf(input15), output15)
 
 })
--- inst/tests/test-json.R
+++ inst/tests/test-json.R
@@ -2,42 +2,44 @@
 context("json i/o")
 
 test_that("test simple conversions to JSON", {
+  tocharjson <- function(...) as.character(toJSON(...))
+
 
   input01 <- list(x="a", y=NA)
-  output01 <- "{ \"x\" : [ \"a\" ], \"y\" : [ null ] }"
-  expect_equal(toJSON(input01), output01)
+  output01 <- "{\"x\":[\"a\"],\"y\":[null]}"
+  expect_equal(tocharjson(input01), output01)
 
   input02 <- list(x=NA, y="a")
-  output02 <- "{ \"x\" : [ null ], \"y\" : [ \"a\" ] }"
-  expect_equal(toJSON(input02), output02)
+  output02 <- "{\"x\":[null],\"y\":[\"a\"]}"
+  expect_equal(tocharjson(input02), output02)
 
   input03 <- list(x=NA, y=1)
-  output03 <- "{ \"x\" : [ null ], \"y\" : [ 1 ] }"
-  expect_equal(toJSON(input03), output03)
+  output03 <- "{\"x\":[null],\"y\":[1]}"
+  expect_equal(tocharjson(input03), output03)
 
   input04 <- list(x=1, y=NA)
-  output04 <- "{ \"x\" : [ 1 ], \"y\" : [ null ] }"
-  expect_equal(toJSON(input04), output04)
+  output04 <- "{\"x\":[1],\"y\":[null]}"
+  expect_equal(tocharjson(input04), output04)
 
   input05 <- c(x="a", y=NA)
-  output05 <- "[ \"a\", null ]"
-  expect_equal(toJSON(input05), output05)
+  output05 <- "[\"a\",null]"
+  expect_equal(tocharjson(input05), output05)
 
   input06 <- c(x=NA, y="a")
-  output06 <- "[ null, \"a\" ]"
-  expect_equal(toJSON(input06), output06)
+  output06 <- "[null,\"a\"]"
+  expect_equal(tocharjson(input06), output06)
 
   input07 <- c(x=1, y=NA)
-  output07 <- "[ 1, \"NA\" ]"
-  expect_equal(toJSON(input07), output07)
+  output07 <- "[1,\"NA\"]"
+  expect_equal(tocharjson(input07), output07)
 
   input08 <- c(x=NA, y=1)
-  output08 <- "[ \"NA\", 1 ]"
-  expect_equal(toJSON(input08), output08)
+  output08 <- "[\"NA\",1]"
+  expect_equal(tocharjson(input08), output08)
 
   # It's a bit of a surprise that NA -> "NA" if numeric and NA -> null if character
-  expect_equal(toJSON(lapply(c(a=1,   b=NA), unbox)), "{ \"a\" : 1, \"b\" : \"NA\" }")
-  expect_equal(toJSON(lapply(c(a="1", b=NA), unbox)), "{ \"a\" : \"1\", \"b\" : null }")
+  expect_equal(tocharjson(lapply(c(a=1,   b=NA), unbox)), "{\"a\":1,\"b\":\"NA\"}")
+  expect_equal(tocharjson(lapply(c(a="1", b=NA), unbox)), "{\"a\":\"1\",\"b\":null}")
 
 })
 

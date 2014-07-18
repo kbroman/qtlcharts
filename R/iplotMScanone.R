7adaedde936b1589fafcae96162cfd2b664b8526
Karl Broman <kbroman@gmail.com>
2014-07-17 23:02:52 -0500

    Revise examples for iplotMScanone

--- R/iplotMScanone.R
+++ R/iplotMScanone.R
@@ -59,12 +59,29 @@
 #' data(grav)
 #' grav <- calc.genoprob(grav, step=1)
 #' grav <- reduce2grid(grav)
-#' out <- scanone(grav, phe=seq(1, nphe(grav), by=5), method="hk")
+#'
+#' # we're going to subset the phenotypes
+#' phecol <- seq(1, nphe(grav), by=5)
+#'
+#' # the times were saved as an attributed
+#' times <- attr(grav, "time")[phecol]
+#'
+#' # genome scan
+#' out <- scanone(grav, phe=phecol, method="hk")
+#'
+#' # plot with qualitative labels on y-axis
 #' iplotMScanone(out, title="iplotMScanone example, no effects")
 #'
+#' # plot with quantitative y-axis
+#' iplotMScanone(out, times=times, title="iplotMScanone example, no effects")
+#'
+#' # estimate QTL effect for each time point at each genomic position
 #' eff <- estQTLeffects(grav, phe=seq(1, nphe(grav), by=5), what="effects")
-#' iplotMScanone(out, effects=eff, title="iplotMScanone example, with effects",
-#'               chartOpts=list(eff_ylab="QTL effect"))
+#'
+#' # plot with QTL effects included (and with quantitative y-axis)
+#' iplotMScanone(out, effects=eff, times=times,
+#'               title="iplotMScanone example, with effects",
+#'               chartOpts=list(eff_ylab="QTL effect", eff_xlab="Time (hrs)"))
 #'
 #' @export
 iplotMScanone <-

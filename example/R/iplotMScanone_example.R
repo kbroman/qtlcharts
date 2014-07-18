3699454e12b4c66d2919b886146349f34531ee71
Karl Broman <kbroman@gmail.com>
2014-07-18 14:09:43 -0500

    Update iplotMScanone example with hr:min times

--- example/R/iplotMScanone_example.R
+++ example/R/iplotMScanone_example.R
@@ -10,11 +10,22 @@ out <- scanone(grav, phe=phecol, method="hk")
 times <- attr(grav, "time")[phecol]
 eff <- estQTLeffects(grav, phe=phecol, what="effects")
 
+to_hrmin <-
+function(times)
+{
+    hr <- floor(times)
+    min <- round((times - hr)*60)
+    if(any(min < 10))
+      min[min < 10] <- paste0("0", min[min < 10])
+
+    paste(hr, min, sep=":")
+}
+
 file <- "../iplotMScanone.html"
 if(file.exists(file)) unlink(file)
 
 iplotMScanone(out, effects=eff, times=times, title="iplotMScanone example",
-              chartOpts=list(eff_ylab="QTL effect", lod_ylab="Time (hrs)"),
+              chartOpts=list(eff_ylab="QTL effect", lod_ylab="Time (hrs)", lod_labels=to_hrmin(times)),
               onefile=TRUE, openfile=FALSE, file=file,
               caption=c("Hover over LOD heat map to view individual curves ",
                 "below and estimated QTL effects to the right.<br><br>\n",

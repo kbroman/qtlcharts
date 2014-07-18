737587d940062f98ffb12caf958c847a29d616eb
Karl Broman <kbroman@gmail.com>
2014-07-17 23:09:48 -0500

    Revise iplotMScanone example to have quantitative y-axis scale on lod heatmap

--- assets/chartexamples/R/iplotMScanone_example.R
+++ assets/chartexamples/R/iplotMScanone_example.R
@@ -5,14 +5,16 @@ library(qtlcharts)
 data(grav)
 grav <- calc.genoprob(grav, step=1)
 grav <- reduce2grid(grav)
-out <- scanone(grav, phe=seq(1, nphe(grav), by=5), method="hk")
-eff <- estQTLeffects(grav, phe=seq(1, nphe(grav), by=5), what="effects")
+phecol <- seq(1, nphe(grav), by=5)
+out <- scanone(grav, phe=phecol, method="hk")
+times <- attr(grav, "time")[phecol]
+eff <- estQTLeffects(grav, phe=phecol, what="effects")
 
 file <- "../iplotMScanone_example.html"
 if(file.exists(file)) unlink(file)
 
-iplotMScanone(out, effects=eff, title="iplotMScanone example, with effects",
-              chartOpts=list(eff_ylab="QTL effect"),
+iplotMScanone(out, effects=eff, times=times, title="iplotMScanone example, with effects",
+              chartOpts=list(eff_ylab="QTL effect", lod_ylab="Time (hrs)"),
               onefile=TRUE, openfile=FALSE, file=file,
               caption=c("Hover over LOD heat map to view individual curves ",
                 "below and estimated QTL effects to the right.<br><br>\n",

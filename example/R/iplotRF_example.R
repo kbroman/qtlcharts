8e4e8b4032c5aa195e71769c47f213a112ddad60
Karl Broman <kbroman@gmail.com>
2014-07-25 10:05:06 -0500

    iplotRF: switch blue and red (red is now _bad_)

--- example/R/iplotRF_example.R
+++ example/R/iplotRF_example.R
@@ -9,4 +9,16 @@ if(file.exists(file)) unlink(file)
 
 iplotRF(badorder, title="iplotRF example",
         onefile=TRUE, openfile=FALSE, file=file,
-        chartOpts=list(pixelPerCell=12))
+        chartOpts=list(pixelPerCell=12),
+        caption = c('The heatmap displays LOD scores indicating linkage between all ',
+                    'pairs of markers. Blue indicates large LOD with rf < 1/2; red ',
+                    'indicates large LOD but with rf > 1/2. ',
+                    'Click on the heatmap to view the corresponding two-locus ',
+                    'genotype table to the right and LOD scores for selected markers, ',
+                    'below. In the subsequent cross-tabulation on right, hover over column ',
+                    'and row headings to view conditional distributions. In the panels below, ',
+                    'hover over points to view marker names and click to refresh the cross-tab ',
+                    'and lower panels with the selected marker.\n',
+                    '</p><hr/><p class="caption"><a style="text-decoration:none;" ',
+                    'href="http://kbroman.org/qtlcharts">R/qtlcharts</a>'))
+

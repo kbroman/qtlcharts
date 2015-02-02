8202d3c56220ebcd429222979a1331efc0786921
Karl Broman <kbroman@gmail.com>
2015-02-02 16:12:33 -0600

    Remove map2json function and change name of corresponding R file (convert_map.R)

--- R/convert_map.R
+++ R/convert_map.R
@@ -1,17 +1,16 @@
-## map2json
+## convert_map
 ## Karl W Broman
 
-# Convert marker map to JSON format
+# Convert marker map to list for use in interactive plot
 #
-# Convert a genetic map to JSON format,
+# Convert a genetic map to format
 # for use with interative graphics, such as \code{\link{iplotMap}}.
 # (Largely for internal use.)
 #
 # @param map An object of class \code{"map"}: a list with each
 #   component being a vector of marker positions
-# @param digits Number of digits in JSON file (passed in \code{\link[jsonlite]{toJSON}}).
 #
-# @return A character string with the input in JSON format.  This
+# @return Map in special format for using in \code{\link{iplotMap}},
 #    includes an ordered vector of chromosome names and then the map,
 #    organized by chromosome and then by marker.
 #
@@ -22,19 +21,7 @@
 # library(qtl)
 # data(hyper)
 # map <- pull.map(hyper)
-# map_as_json <- map2json(map)
-map2json <-
-function(map, digits=4) {
-    chrnames <- names(map)
-    # force use of hash with single numeric values
-    map <- lapply(map, function(a) lapply(a, jsonlite::unbox))
-
-    mnames <- unlist(lapply(map, names))
-    names(mnames) <- NULL
-
-    strip_whitespace( jsonlite::toJSON(list(chr=chrnames, map=map, markernames=mnames), digits=digits, na="null") )
-}
-
+# map_reformatted <- convert_map(map)
 convert_map <-
 function(map) {
     chrnames <- names(map)

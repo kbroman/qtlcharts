becb1f3b377dae9adf8cac7ee7a9694b1a764f9c
Karl Broman <kbroman@gmail.com>
2015-02-02 21:25:24 -0600

    Revise chartOpts tests (focus solely on add2chartOpts)

--- tests/testthat/test-chartopts.R
+++ tests/testthat/test-chartopts.R
@@ -1,82 +1,40 @@
 context("chartOpts")
 
-tmpf <- function(opts, digits=2) as.character(jsonlite::toJSON(opts4json(opts), digits=digits))
-addquotes <- function(x) paste0("\"", x, "\"")
+test_that("add2chartOpts works correctly", {
 
-test_that("conversion of chartOpts to JSON", {
+    # adding something to a NULL
+    expect_equal(list(height=500), add2chartOpts(NULL, height=500))
 
-    expect_equal(tmpf(NULL), "null")
+    # adding two things to a NULL
+    expect_equal(list(height=500, width=1000),
+                 add2chartOpts(NULL, height=500, width=1000))
 
-    input01 <- c(xlab="x axis label", ylab="y axis label")
-    output01 <- "{\"xlab\":\"x axis label\",\"ylab\":\"y axis label\"}"
-    expect_equal(tmpf(input01), output01)
+    # adding something to a list
+    expect_equal(list(title="title", xlab="xlab", height=300),
+                 add2chartOpts(list(title="title", xlab="xlab"), height=300))
 
-    input02 <- list(xlab="x axis label", ylab="y axis label")
-    output02 <- output01
-    expect_equal(tmpf(input02), output02)
+    # adding two things to a list
+    expect_equal(list(title="title", xlab="xlab", height=300, width=600),
+                 add2chartOpts(list(title="title", xlab="xlab"), height=300, width=600))
 
-    input03 <- list(xlab="x axis label", ylab="y axis label", zlab=NULL, margins=list(left=2, right=50))
-    output03 <- "{\"xlab\":\"x axis label\",\"ylab\":\"y axis label\",\"zlab\":null,\"margins\":{\"left\":2,\"right\":50}}"
-    expect_equal(tmpf(input03), output03)
+    # adding something that's already there
+    expect_equal(list(title="title", height=300),
+                 add2chartOpts(list(title="title", height=300), height=600))
+    expect_equal(list(height=300, title="title"),
+                 add2chartOpts(list(height=300, title="title"), height=600))
+    expect_equal(list(title="title", height=300, width=500),
+                 add2chartOpts(list(title="title", height=300), height=600, width=500))
+    expect_equal(list(height=300, title="title", width=500),
+                 add2chartOpts(list(height=300, title="title"), width=500, height=600))
 
-})
+    # adding two things that are already there
+    expect_equal(list(title="title", height=300, width=500),
+                 add2chartOpts(list(title="title", height=300, width=500), height=600, width=1000))
+    expect_equal(list(title="title", height=300, width=500),
+                 add2chartOpts(list(title="title", height=300, width=500), width=1000, height=600))
+    expect_equal(list(title="title", height=300, width=500, xlab="xlab"),
+                 add2chartOpts(list(title="title", height=300, width=500),
+                               height=600, width=1000, xlab="xlab"))
 
-test_that("use of add2chartOpts", {
-
-    input04 <- list(x="x", y="y")
-    input04 <- add2chartOpts(input04, chartdivid="chart")
-    output04 <- "{\"x\":\"x\",\"y\":\"y\",\"chartdivid\":\"chart\"}"
-    expect_equal(tmpf(input04), output04)
-
-    input05 <- c(x="x", y="y")
-    input05 <- add2chartOpts(input05, chartdivid="chart")
-    output05 <- output04
-    expect_equal(tmpf(input05), output05)
-
-    input06 <- list(x="x", y="y")
-    input06 <- add2chartOpts(input06, title="blah", chartdivid="chart")
-    output06 <- "{\"x\":\"x\",\"y\":\"y\",\"title\":\"blah\",\"chartdivid\":\"chart\"}"
-    expect_equal(tmpf(input06), output06)
-
-    input07 <- c(x="x", y="y")
-    input07 <- add2chartOpts(input07, title="blah", chartdivid="chart")
-    output07 <- output06
-    expect_equal(tmpf(input07), output07)
-
-})
-
-test_that("small things", {
-
-    input08 <- list(x="a", y=NA)
-    output08 <- "{\"x\":\"a\",\"y\":null}"
-    expect_equal(tmpf(input08), output08)
-
-    input09 <- list(x=NA, y="a")
-    output09 <- "{\"x\":null,\"y\":\"a\"}"
-    expect_equal(tmpf(input09), output09)
-
-    input10 <- list(x=1, y=NA)
-    output10 <- "{\"x\":1,\"y\":null}"
-    expect_equal(tmpf(input10), output10)
-
-    input11 <- list(x=NA, y=1)
-    output11 <- "{\"x\":null,\"y\":1}"
-    expect_equal(tmpf(input11), output11)
-
-    input12 <- c(x="a", y=NA)
-    output12 <- output08
-    expect_equal(tmpf(input12), output12)
-
-    input13 <- c(x=NA, y="a")
-    output13 <- output09
-    expect_equal(tmpf(input13), output13)
-
-    input14 <- c(x=1, y=NA)
-    output14 <- "{\"x\":1,\"y\":\"NA\"}"
-    expect_equal(tmpf(input14), output14)
-
-    input15 <- c(x=NA, y=1)
-    output15 <- "{\"x\":\"NA\",\"y\":1}"
-    expect_equal(tmpf(input15), output15)
 
 })

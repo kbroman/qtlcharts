0460b61683d8519bb343ef1a8813795e193e2ff4
Karl Broman <kbroman@gmail.com>
2016-05-13 19:33:41 -0400

    include a missing value in tests of iplotCurves with just curves

--- tests/htmltest/iplotCurves_justcurves.R
+++ tests/htmltest/iplotCurves_justcurves.R
@@ -1,4 +1,7 @@
-theplot <- iplotCurves(grav$pheno, times,
+# plug in a missing value
+phe <- grav$pheno[14,ncol(grav$pheno)] <- NA
+
+theplot <- iplotCurves(phe, times,
                        chartOpts=list(curves_xlab="Time", curves_ylab="Root tip angle"))
 
 htmlwidgets::saveWidget(theplot, file="iplotCurves_justcurves.html", selfcontained=FALSE)

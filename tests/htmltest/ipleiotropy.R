b5a4b9109ef634cb89784e824ce1c23dc9170a62
Karl Broman <kbroman@gmail.com>
2017-05-14 09:56:19 -0500

    Add tests of ipleiotropy restricted to an interval

--- tests/htmltest/ipleiotropy.R
+++ tests/htmltest/ipleiotropy.R
@@ -3,3 +3,7 @@ htmlwidgets::saveWidget(theplot, file="ipleiotropy.html", selfcontained=FALSE)
 
 theplot_nolod <- ipleiotropy(grav, pheno.col=c(32, 49), chr=1)
 htmlwidgets::saveWidget(theplot_nolod, file="ipleiotropy_nolod.html", selfcontained=FALSE)
+
+theplot_interval <- ipleiotropy(grav, out.hk, lodcolumn=1:2, pheno.col=c(32, 49), chr=1,
+                                interval=c(53,68))
+htmlwidgets::saveWidget(theplot_interval, file="ipleiotropy_interval.html", selfcontained=FALSE)

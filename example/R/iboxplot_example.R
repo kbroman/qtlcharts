78991027ce3310629e8e1e3f65b5570ab9bd14e4
Karl Broman <kbroman@gmail.com>
2017-06-01 13:35:08 -0500

    Update iboxplot example

--- example/R/iboxplot_example.R
+++ example/R/iboxplot_example.R
@@ -9,16 +9,20 @@ med <- apply(exdat, 1, median, na.rm=TRUE)
 exdat <- exdat[order(med, decreasing=TRUE),]
 
 # remove the target file, if it exists
-file <- "../iboxplot.html"
+file <- "iboxplot.html"
 if(file.exists(file)) unlink(file)
 
-# onefile=TRUE makes the resulting html file all-inclusive (javascript + css + data)
-#     this is a bit wasteful of space, but it's easy
-iboxplot(exdat, title="iboxplot example", chartOpts=list(xlab="Mice", ylab="Gene expression"),
-         orderByMedian=FALSE, onefile=TRUE, openfile=FALSE, file=file,
-         qu=c(0.01, 0.1, 0.25),
-         caption=c("The top panel is like a set of 494 box plots: lines are drawn at a series ",
-           "of percentiles for each of the distributions. Hover over a column in the top panel ",
-           "and the corresponding distribution is show below; click for it to persist; click ",
-           "again to make it go away.<br><br>\n",
-           "</p><hr/><p class=\"caption\"><a style=\"text-decoration:none;\" href=\"http://kbroman.org/qtlcharts\">R/qtlcharts</a>"))
+footer <- paste(readLines("footer.txt"), collapse="\n")
+
+qtlcharts::setScreenSize("normal")
+theplot <- iboxplot(exdat, chartOpts=list(xlab="Mice", ylab="Gene expression",
+                                          heading="<code>iboxplot</code>",
+                                          caption=paste("<b><code>iboxplot</code> example:</b>",
+                                                        "The top panel is like a set of 494 box plots: lines are drawn at a series ",
+                                                        "of percentiles for each of the distributions. Hover over a column in the top panel ",
+                                                        "and the corresponding distribution is show below; click for it to persist; click ",
+                                                        "again to make it go away."),
+                                          footer=footer),
+                    orderByMedian=FALSE, qu=c(0.01, 0.1, 0.25))
+htmlwidgets::saveWidget(theplot, file=file, selfcontained=TRUE)
+file.rename(file, file.path("..", file))

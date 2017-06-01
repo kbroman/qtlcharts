089c8d0d2e5e85648f6a477a64d4ad07c7147a4b
Karl Broman <kbroman@gmail.com>
2017-06-01 14:34:12 -0500

    Update iplotScantwo example

--- example/R/iplotScantwo_example.R
+++ example/R/iplotScantwo_example.R
@@ -1,4 +1,4 @@
-# example of iplotScanone
+# example of iplotScantwo
 
 library(qtlcharts)
 library(qtl)
@@ -8,15 +8,20 @@ hyper <- hyper[c(1,4,6,15),]
 hyper <- calc.genoprob(hyper, step=2)
 out2 <- scantwo(hyper, method="em", verbose=FALSE)
 
-file <- "../iplotScantwo.html"
+file <- "iplotScantwo.html"
 if(file.exists(file)) unlink(file)
 
-iplotScantwo(out2, hyper,
-             title="iplotScantwo example",
-             onefile=TRUE, openfile=FALSE, file=file,
-             caption=c('Use the drop-down menus to select the LOD scores to plot. ',
-                       'Hover over the heatmap to view the LOD scores; click to view cross-sectional ',
-                       'slices below and QTL effects plots to the right.<br><br>\n',
-                       '</p><hr/><p class="caption"><a style="text-decoration:none;" ',
-                       'href="http://kbroman.org/qtlcharts">R/qtlcharts</a>'),
-             chartOpts=list(axispos=c(xtitle=25,ytitle=40,xlabel=5,ylabel=5)))
+footer <- paste(readLines("footer.txt"), collapse="\n")
+
+qtlcharts::setScreenSize(height=800, width=1200)
+theplot <- iplotScantwo(out2, hyper, digits=3,
+                        chartOpts=list(heading="<code>iplotScantwo</code>",
+                                       caption=paste("<b><code>iplotScantwo</code> example:</b>",
+                                                     'Use the drop-down menus to select the LOD scores to plot. ',
+                                                     'Hover over the heatmap to view the LOD scores; click to view cross-sectional ',
+                                                     'slices below and QTL effects plots to the right.'),
+                                       footer=footer,
+                                       axispos=c(xtitle=25,ytitle=40,xlabel=5,ylabel=5)))
+
+htmlwidgets::saveWidget(theplot, file=file, selfcontained=TRUE)
+file.rename(file, file.path("..", file))

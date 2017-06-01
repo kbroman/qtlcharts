315adeb37672f4cbbbe6dabee1b871e0d8dab0ea
Karl Broman <kbroman@gmail.com>
2017-06-01 13:47:10 -0500

    Update iplotCorr example

--- example/R/iplotCorr_example.R
+++ example/R/iplotCorr_example.R
@@ -3,16 +3,22 @@
 library(qtlcharts)
 data(geneExpr)
 
-file <- "../iplotCorr.html"
+file <- "iplotCorr.html"
 if(file.exists(file)) unlink(file)
 
-iplotCorr(geneExpr$expr, geneExpr$genotype, title = "iplotCorr example",
-          chartOpts=list(cortitle="Correlation matrix",
-            scattitle="Scatterplot"),
-          onefile=TRUE, openfile=FALSE, file=file,
-          caption=c("The left panel is an image of a correlation matrix, with ",
-            "blue = -1 and red = +1. Hover over pixels in the correlation matrix ",
-            "on the left to see the values; click to see the corresponding ",
-            "scatterplot on the right.<br><br>\n",
-            "</p><hr/><p class=\"caption\"><a style=\"text-decoration:none;\" href=\"http://kbroman.org/qtlcharts\">R/qtlcharts</a>"))
+footer <- paste(readLines("footer.txt"), collapse="\n")
 
+qtlcharts::setScreenSize("normal")
+theplot <- iplotCorr(geneExpr$expr, geneExpr$genotype, reorder=TRUE,
+                     chartOpts=list(cortitle="Correlation matrix",
+                                    scattitle="Scatterplot",
+                                    heading="<code>iplotCorr</code>",
+                                    footer=footer,
+                                    caption=paste("<b><code>iplotCorr</code> example:</b>",
+                                                  "The left panel is an image of a correlation matrix, with",
+                                                  "blue = -1 and red = +1. Hover over pixels in the correlation matrix",
+                                                  "on the left to see the values; click to see the corresponding",
+                                                  "scatterplot on the right.")))
+
+htmlwidgets::saveWidget(theplot, file=file, selfcontained=TRUE)
+file.rename(file, file.path("..", file))

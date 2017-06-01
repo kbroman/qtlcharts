c3b472e3a58cc319e0a5e3d586fd8e9d2bddc36e
Karl Broman <kbroman@gmail.com>
2017-06-01 14:24:37 -0500

    Update iplotRF example

--- example/R/iplotRF_example.R
+++ example/R/iplotRF_example.R
@@ -5,20 +5,26 @@ library(qtl)
 data(badorder)
 rf <- pull.rf(badorder)
 
-file <- "../iplotRF.html"
+file <- "iplotRF.html"
 if(file.exists(file)) unlink(file)
 
-iplotRF(badorder, title="iplotRF example",
-        onefile=TRUE, openfile=FALSE, file=file,
-        chartOpts=list(pixelPerCell=12),
-        caption = c('The heatmap displays LOD scores indicating linkage between all ',
-                    'pairs of markers. Blue indicates large LOD with rf < 1/2; red ',
-                    'indicates large LOD but with rf > 1/2. ',
-                    'Click on the heatmap to view the corresponding two-locus ',
-                    'genotype table to the right and LOD scores for selected markers, ',
-                    'below. In the subsequent cross-tabulation on right, hover over column ',
-                    'and row headings to view conditional distributions. In the panels below, ',
-                    'hover over points to view marker names and click to refresh the cross-tab ',
-                    'and lower panels with the selected marker.\n',
-                    '</p><hr/><p class="caption"><a style="text-decoration:none;" ',
-                    'href="http://kbroman.org/qtlcharts">R/qtlcharts</a>'))
+footer <- paste(readLines("footer.txt"), collapse="\n")
+
+qtlcharts::setScreenSize(height=800, width=1200)
+theplot <- iplotRF(badorder,
+                   chartOpts=list(pixelPerCell=12,
+                                  heading = "<code>iplotRF</code>",
+                                  caption = paste("<b><code>iplotRF</code> example:</b>",
+                                                  'The heatmap displays LOD scores indicating linkage between all ',
+                                                  'pairs of markers. Blue indicates large LOD with rf < 1/2; red ',
+                                                  'indicates large LOD but with rf > 1/2. ',
+                                                  'Click on the heatmap to view the corresponding two-locus ',
+                                                  'genotype table to the right and LOD scores for selected markers, ',
+                                                  'below. In the subsequent cross-tabulation on right, hover over column ',
+                                                  'and row headings to view conditional distributions. In the panels below, ',
+                                                  'hover over points to view marker names and click to refresh the cross-tab ',
+                                                  'and lower panels with the selected marker.'),
+                                  footer=footer))
+
+htmlwidgets::saveWidget(theplot, file=file, selfcontained=TRUE)
+file.rename(file, file.path("..", file))

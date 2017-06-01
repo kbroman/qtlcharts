4f1ec1ec962ba04a73d1aafbf93efb131c0eb107
Karl Broman <kbroman@gmail.com>
2017-06-01 13:37:17 -0500

    Update ipleiotropy example

--- example/R/ipleiotropy_example.R
+++ example/R/ipleiotropy_example.R
@@ -10,8 +10,16 @@ out <- scanone(grav, phe=c(156, 241), method="hk")
 file <- "ipleiotropy.html"
 if(file.exists(file)) unlink(file)
 
+footer <- paste(readLines("footer.txt"), collapse="\n")
+
+qtlcharts::setScreenSize("normal")
 theplot <- ipleiotropy(grav, out, chr=1, phe=c(156, 241), lod=1:2,
-                       chartOpts=list(title="ipleiotropy example"), digits=3)
+                       chartOpts=list(heading="<code>ipleiotropy</code>",
+                                      footer=footer,
+                                      caption=paste("<b><code>ipleiotropy</code> example:</b>",
+                                                    "Use the double-slider below the LOD curve plot to select two QTL positions,",
+                                                    "whose genotypes will then be used to color the points in the right panel.")),
+                       digits=3)
 htmlwidgets::saveWidget(theplot, file=file, selfcontained=TRUE)
 
 file.rename(file, file.path("..", file))
